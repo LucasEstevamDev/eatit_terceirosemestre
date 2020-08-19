a50d949685c326ba681f6e8aa64576d7ecee7683
EstevamDev <lucas.estevam@outlook.com.br>
2020-08-19 19:02:14 -0300

    Padronização e Inclusão de janelas do tipo MODAL no logout

--- js/script.js
+++ js/script.js
@@ -23,6 +23,16 @@ function Login() {
     return false;
 }
 
+/* FUNÇÃO PARA FORMATAR CAMPO MOEDA */
+function k(i) {
+    var v = i.value.replace(/\D/g, '');
+    v = (v / 100).toFixed(2) + '';
+    v = v.replace(".", ",");
+    v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+    v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+    i.value = v;
+}
+
 /* FUNÇÃO DE VALIDAÇÃO DO FORMULÁRIO DE CADASTRO */
 function validaCadastro() {
     var senha = document.getElementById('inputSenha').value,
